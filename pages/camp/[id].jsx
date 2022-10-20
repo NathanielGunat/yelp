308b02b3fd248167fbc8d3b7412b87f71bfe305a
nathangunat <nathangunat@gmail.com>
2022-10-20 17:25:19 +0100

    dynamic route

--- pages/camp/[id].jsx
+++ pages/camp/[id].jsx
@@ -1,11 +1,25 @@
 import { useRouter } from "next/router";
 import { CAMPS } from "../../DATA/DATA";
-import { Camp } from "../../components/ui";
-
+import Image from "next/image";
+import { Text } from "@components/ui";
+import mapPlaceHolder from "@public/Map.png";
 export default function Slug() {
   const route = useRouter();
   const { id } = route.query;
-  const Camp = CAMPS.find((camp) => camp.id === id);
+  const camp = CAMPS.find((camp) => camp.id === id);
 
-  return;
+  return (
+    <div className="shadow-md p-4 space-y-5 lg:flex lg:flex-row-reverse">
+      <section>
+        <div className="relative aspect-video">
+          <Image src={camp.src} alt={camp.name} layout="fill" />
+        </div>
+        <Text variant={"sectionHeading"}>{camp.name}</Text>
+        <p>{camp.discription}</p>
+      </section>
+      <section>
+        <Image src={mapPlaceHolder} alt="" />
+      </section>
+    </div>
+  );
 }
